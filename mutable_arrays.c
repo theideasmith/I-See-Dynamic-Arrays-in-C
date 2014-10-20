643536c559f9af9696571fb5e10faf3f043c2dad
aclinnovator <akivalipshitz@Akivas-Air.home>
2014-10-19 21:49:34 -0400

    First version of permute function made by myself

--- mutable_arrays.c
+++ mutable_arrays.c
@@ -7,21 +7,13 @@ int main()
     PRINT_STR("Please enter a number");
     int ttlLoops = scanInt(); //Total Number of loops run
     int count = 0; //How many times the loop has run in total
-    int **tu_D = init_2d_Array();
     //LOOP A
-    for (int x = 0; x < ttlLoops;x++) {
-        one_D_Array ar2 = init_one_D_Array(); //Initializing a one_D_Array
+  one_D_Array ar2 = init_one_D_Array(); //Initializing a one_D_Array
         
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < ttlLoops; i++) {
             ar2 = addToArray(ar2, i); //Adding to the one dimensional array an int
         }
-        tu_D = addArrayToArray(tu_D, ar2.array, count); //Adding the one dimensional array to a 2d array
-        printf("\n<<%d:",count);
-        print_Array(tu_D[count], 9); //Printing the 1D array just saved above into the 2D array
-        PRINT_STR(">>\n");
-        count ++;
-        
-    }
+
     //LOOP A
     //LOOP B
     // for (int i =0; i < count; i++) {
@@ -31,15 +23,15 @@ int main()
     //     countOfArray(tu_D[i]);
     //     PRINT_STR(">>\n");
     // }
-    two_D_Array addedTo= init_2D_Struct();
+/*    two_D_Array addedTo= init_2D_Struct();
     one_D_Array toAdd = init_one_D_Array();
     addToArray(toAdd,4);
 
     add_To_2D_Struct(toAdd,addedTo);
 
     print_2D_Struct(addedTo,1);
-    PRINT_STR("\n");
-
+    PRINT_STR("\n");*/
+    permute(ar2.array,ttlLoops);
     return 0;
     //LOOP B
 }
--- mutable_arrays.h
+++ mutable_arrays.h
@@ -13,8 +13,6 @@ typedef struct  {
     one_D_Array *array; 
 }two_D_Array;
 
-
-
 //##################Helper Functions#############################
 
 void printVal(int val);
@@ -61,7 +59,13 @@ int countOfArray(int *ar){
     printVal(sizf);
     return sizf;
 }
-
+int factorial(int i){
+    int current_TTL = i;
+    for (int x = i-1; x > 0; x--) {
+        current_TTL*=x;
+    }
+    return current_TTL;
+}
 int scanInt(){
     int begin;
     scanf("%d",&begin);
@@ -144,4 +148,56 @@ void print_2D_Struct(two_D_Array toPrint, int untilIndx){
         print_Array(((one_D_Array) toPrint.array[i]).array,((one_D_Array) toPrint.array[i]).lastIndx);
     }
 
+}
+
+int *swap(int *array, int ind1, int ind2) //Swaps the location of two arrays
+{
+    int v1 = array[ind1];
+    int v2 = array[ind2]; 
+    array[ind2] = v1;
+    array[ind1] = v2; 
+
+    return array;
+}
+void permute(int *toPermute, int elementCount) {
+    int ttLoops = factorial(elementCount);
+    printVal(ttLoops); PRINT_STR("\n");
+    int currentLoop = 0;
+    int currentIndx = elementCount-1;
+    while (currentLoop<ttLoops){
+        toPermute = swap(toPermute, currentIndx, elementCount-1);
+        print_Array(toPermute,elementCount-1);
+        PRINT_STR("\n");
+        currentLoop++;
+        currentIndx = currentIndx == 0 ? elementCount -1:currentIndx-1;
+    }
+    /*---(OUTPUT)----
+    Not as I expected. The process used does get the string back to its original form, 
+    but it does not produce every permutation. I think I'll need to implement the shift every
+    time the counter produces the set of outputs for a single first digit. 
+    - As you will notice, the functions correctly permutes an array of length 3, but not four. 
+Digits:  3  |  4
+        123 | 1234 
+        132 | 1234
+        213 | 1342
+        231 | 1342
+        312 | 2314
+        321 | 2314
+            | 2341
+              2341
+              2413
+              2413
+              3124
+              3124
+              3412
+              3412
+              3421
+              3421
+              4123
+              4123
+              4132
+              4132
+              4231
+              4231
+    --------------*/
 }
